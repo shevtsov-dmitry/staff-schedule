9b474ad0efa9c06af4ee66b5db59cc5be6b37daa
shado <shadowstakeus28@gmail.com>
2023-05-12 13:19:03 +0300

    feat: select all data from table and show on table

--- web/main.js
+++ web/main.js
@@ -3,15 +3,16 @@ import 'handsontable/dist/handsontable.full.min.css';
 
 const container = document.querySelector('#example');
 
-const hot = new Handsontable(container, {
-  data: [
-    ['', 'Tesla', 'Volvo', 'Toyota', 'Ford'],
-    ['2019', 10, 11, 12, 13],
-    ['2020', 20, 11, 14, 13],
-    ['2021', 30, 15, 12, 13]
-  ],
-  rowHeaders: true,
-  colHeaders: true,
-  height: 'auto',
-  licenseKey: 'non-commercial-and-evaluation' // for non-commercial use only
-});
+const save_btn = document.querySelector('#save')
+
+fetch('http://localhost:3000/api/data')
+  .then(response => response.json())
+  .then(data => {
+    const hot = new Handsontable(container, {
+      data: data,
+      rowHeaders: true,
+      colHeaders: true,
+      height: 'auto',
+      licenseKey: 'non-commercial-and-evaluation' // for non-commercial use only
+    });
+  });
