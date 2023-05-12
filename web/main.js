c5adb45801748892c0712b78c17ad23a2b48e5d2
shado <shadowstakeus28@gmail.com>
2023-05-12 13:30:08 +0300

    feat: add empty row on btn click

--- web/main.js
+++ web/main.js
@@ -1,18 +1,33 @@
 import Handsontable from 'handsontable';
 import 'handsontable/dist/handsontable.full.min.css';
 
+const host = 'http://localhost:3000'
+
 const container = document.querySelector('#example');
+const add_row_btn = document.querySelector('.add-row')
 
-const save_btn = document.querySelector('#save')
-
-fetch('http://localhost:3000/api/data')
+fetch(`${host}/api/data`)
   .then(response => response.json())
   .then(data => {
-    const hot = new Handsontable(container, {
-      data: data,
-      rowHeaders: true,
-      colHeaders: true,
-      height: 'auto',
-      licenseKey: 'non-commercial-and-evaluation' // for non-commercial use only
-    });
+
+  // create table from fetch data
+  const hot = new Handsontable(container, {
+    data: data,
+    rowHeaders: true,
+    colHeaders: true,
+    height: 'auto',
+    licenseKey: 'non-commercial-and-evaluation' // for non-commercial use only
   });
+
+  // add new empty row
+  add_row_btn.addEventListener('click',()=>{
+    fetch(`${host}/add-empty-row`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+    location.reload()
+  });
+
+});
