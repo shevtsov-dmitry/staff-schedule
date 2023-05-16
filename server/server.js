27c9260a84e4d35610cfa8894fd57467a7522b54
shado <shadowstakeus28@gmail.com>
2023-05-16 12:49:43 +0300

    style: initial user interface

--- server/server.js
+++ server/server.js
@@ -5,13 +5,13 @@ const app = express();
 
 // access between web and server
 const cors = require('cors')
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "http://localhost:5173"); // update with your actual origin
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "http://localhost:5173"); 
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
-  });
+});
 
-  // * ------ CLIENT CONNECT TO DB ------
+// * ------ CLIENT CONNECT TO DB ------
 const client = new Client({
     user: 'me',
     host: 'localhost',
@@ -24,39 +24,39 @@ client.connect()
 
 // *** ------ GETS ------
 // GET * table names from database
-app.get('/get-table-names',(req,res)=>{
+app.get('/get-table-names', (req, res) => {
     const query = `SELECT table_name
     FROM information_schema.tables 
     WHERE table_schema = 'public' 
     AND table_catalog = '${client.database}'
     AND table_type = 'BASE TABLE';`
-    client.query(query, (err,result)=>{
+    client.query(query, (err, result) => {
         res.send(result.rows)
     })
 })
 
 // GET * column names from table
-app.get('/get-column-names', (req,res)=>{
+app.get('/get-column-names', (req, res) => {
     const table_name = req.query.table
     const query = `SELECT column_name
     FROM information_schema.columns
     WHERE table_name = '${table_name}';`
-    client.query(query, (err, result) =>{
+    client.query(query, (err, result) => {
         if (err) {
             console.error(err);
             res.status(500).send('Error retrieving column names');
         }
-        else{
+        else {
             const rows = result.rows
             res.send(rows)
         }
     })
 })
 // GET * rows
-app.get('/get-all-data-from-table', (req,res) => {
-    const table_name = req.query.table 
+app.get('/get-all-data-from-table', (req, res) => {
+    const table_name = req.query.table
     const query = `SELECT * FROM ${table_name}`
-    client.query(query, (err, result)=>{
+    client.query(query, (err, result) => {
         const rows = result.rows
         res.send(rows)
     })
@@ -64,19 +64,25 @@ app.get('/get-all-data-from-table', (req,res) => {
 
 // *** ------ POSTS ------
 // add row
-app.post('/add-empty-row',(req,res)=>{
+app.post('/add-empty-row', (req, res) => {
     const table_name = req.query.table
+    console.log("****************\n" + table_name)
     client.query(`INSERT INTO ${table_name} DEFAULT VALUES`)
 })
 
 app.use(bodyParser.json())
-app.post('/delete-selected-rows', (req,res) =>{
+app.post('/delete-selected-rows', (req, res) => {
     const recievedArray = req.body
     const start = recievedArray[0]
     const end = recievedArray[1]
     // TODO const table_name = recievedArray[3]
     const query = `DELETE FROM department WHERE department_id BETWEEN ${start} AND ${end}`
     client.query(query)
+})
+
+app.post('/save-table', (req, res) => {
+    const query_clear_table = "DELETE FROM"
+    const table = req.body
 
 })
 
--- web/main.js
+++ web/main.js
@@ -15,130 +15,134 @@ const hot = new Handsontable(container, {
 // fill the list with values which will be chosen by user to display certain table
 const table_names_list = document.querySelector('.table-names-list')
 
-async function main(){
-
+async function main() {
   // *1. get all table names
   table_names_list.innerHTML = "";
   fetch(`${host}/get-table-names`, {
     method: "GET",
-    headers: {'Content-Type' : 'application/json '},
+    headers: { 'Content-Type': 'application/json ' },
   })
-  .then(response => response.json())
-  .then(data => {
+    .then(response => response.json())
+    .then(data => {
 
-    // *2. show recieved data as list
-    for (const key in data) {
-      if (Object.hasOwnProperty.call(data, key)) {
-        const element = data[key];
-        table_names_list.innerHTML += `<li>${element.table_name}</li>`
+      // *2. show recieved data as list
+      for (const key in data) {
+        if (Object.hasOwnProperty.call(data, key)) {
+          const element = data[key];
+          table_names_list.innerHTML += `<li>${element.table_name}</li>`
+        }
       }
-    }
 
-    // *3. For each name add event listener to make action to run
-    // *   function @constructTable(), which will create table,
-    // *   fill it with data, show column names, make interactions
-    // *   to add, delete, change rows and save it in database
-    for (const name of table_names_list.children) {
-      name.addEventListener('click', ()=> {
-        constructTable(name.textContent)
-      })
-    }
-    
-  })
+      // *3. For each name add event listener to make action to run
+      // *   function @constructTable(), which will create table,
+      // *   fill it with data, show column names, make interactions
+      // *   to add, delete, change rows and save it in database
+      for (const name of table_names_list.children) {
+        name.addEventListener('click', () => {
+          constructTable(name.textContent)
+        })
+      }
+
+
+    })
 }
 
 const save_btn = document.querySelector('#save')
 const add_row_btn = document.querySelector('.add-row')
 const remove_row_btn = document.querySelector('.remove-row')
 
-async function constructTable(table_name){
-  fetch(`${host}/get-all-data-from-table?table=${table_name}`,{
+async function constructTable(table_name) {
+  fetch(`${host}/get-all-data-from-table?table=${table_name}`, {
     method: 'GET',
-    headers: {'Content-Type': 'application/json'}
+    headers: { 'Content-Type': 'application/json' }
   })
-  .then(response => response.json())
-  .then(async data => {
-  // TODO fix hardcoded "department table"
-  await getColumnNames(table_name)
-  .then(col_names_array => {
-    // add data and column headers to table
-    setTimeout(() => {
-      hot.updateSettings({
-        data: data,
-        colHeaders: col_names_array
-      })  
-    }, 200);
-    
-  })
-  // TODO save all, which was chanded by user in table
-  // preset send all data from table
-  // save_btn.addEventListener('click',()=>{
-  //   const data = hot.getData()
-  //   const xhr = new XMLHttpRequest()
-  //   xhr.open('POST', 'http://localhost:3000/saveData')
-  //   xhr.setRequestHeader('Content-Type', 'application/json')
-  //   xhr.send(JSON.stringify(data))
-  // })
+    .then(response => response.json())
+    .then(async data => {
+      await getColumnNames(table_name)
+        .then(col_names_array => {
+          // add data and column headers to table
+          setTimeout(() => {
+            hot.updateSettings({
+              data: data,
+              colHeaders: col_names_array
+            })
+          }, 200);
+        })
 
-  // * REMOVE selected rows
-  let selectedRows = [];
-  remove_row_btn.addEventListener("mouseenter",  () => {
-    selectedRows = hot.getSelected()
-  });
-  remove_row_btn.addEventListener('click', ()=>{
-    deleteSelectedRows(selectedRows)
-    location.reload()
-  })
-});
+      // @addEmptyRow() is event listener for button
+      await addEmptyRow(table_name)
+      // TODO save all, which was chanded by user in table
+      // preset send all data from table
+      // save_btn.addEventListener('click',()=>{
+      //   const data = hot.getData()
+      //   const xhr = new XMLHttpRequest()
+      //   xhr.open('POST', 'http://localhost:3000/saveData')
+      //   xhr.setRequestHeader('Content-Type', 'application/json')
+      //   xhr.send(JSON.stringify(data))
+      // })
+
+      // * ADD new empty row
+      async function addEmptyRow(table_name) {
+        add_row_btn.addEventListener('click', () => {
+          fetch(`${host}/add-empty-row?table=${table_name}`, {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json'
+            }
+          })
+          hot.alter('insert_row_below', hot.countRows())
+        });
+      }
+      // * REMOVE selected rows
+      let selectedRows = [];
+      remove_row_btn.addEventListener("mouseenter", () => {
+        selectedRows = hot.getSelected()
+      });
+      remove_row_btn.addEventListener('click', () => {
+        deleteSelectedRows(selectedRows)
+        location.reload()
+      })
+    });
 }
 
-function deleteSelectedRows(selectedRows){
+function deleteSelectedRows(selectedRows) {
   // define and send two id values. between them content will be removed 
   let start_row = selectedRows[0][0]
   let end_row = selectedRows[0][2]
-  let id_value_start = hot.getDataAtCell(start_row,0)
-  let id_value_end = hot.getDataAtCell(end_row,0)
-  let array_to_send = [id_value_start,id_value_end]
+  let id_value_start = hot.getDataAtCell(start_row, 0)
+  let id_value_end = hot.getDataAtCell(end_row, 0)
+  let array_to_send = [id_value_start, id_value_end]
 
-  fetch(`${host}/delete-selected-rows`,{
+  fetch(`${host}/delete-selected-rows`, {
     method: 'POST',
-    headers: {'Content-Type' : 'application/json '},
+    headers: { 'Content-Type': 'application/json ' },
     body: JSON.stringify(array_to_send)
   })
 }
 
-// add new empty row
-async function addEmptyRow(table_name){
-  add_row_btn.addEventListener('click',()=>{
-    fetch(`${host}/add-empty-row?table=${table_name}`, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    })
-    hot.alter('insert_row_below', hot.countRows())
-  });
-}
+
 
 // get column names
-async function getColumnNames(table_name){
+async function getColumnNames(table_name) {
   let columns_names_array = []
-  fetch(`${host}/get-column-names?table=${table_name}`,{
+  fetch(`${host}/get-column-names?table=${table_name}`, {
     method: 'GET',
-    headers: {'Content-Type' : 'application/json '},
+    headers: { 'Content-Type': 'application/json ' },
   })
-  .then(response => response.json())
-  .then(columns_names_object => {
-    for (const key in columns_names_object) {
-      if (Object.hasOwnProperty.call(columns_names_object, key)) {
-        const element = columns_names_object[key];
-        columns_names_array.push(element.column_name)
+    .then(response => response.json())
+    .then(columns_names_object => {
+      for (const key in columns_names_object) {
+        if (Object.hasOwnProperty.call(columns_names_object, key)) {
+          const element = columns_names_object[key];
+          columns_names_array.push(element.column_name)
+        }
+        else throw new Error(`Object does not have property ${key}`)
       }
-      else throw new Error(`Object does not have property ${key}`)
-    }
-  })
+    })
   return columns_names_array
 }
+
+
 
 
 await main()
