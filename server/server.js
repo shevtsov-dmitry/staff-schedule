85840d31ee28a28376655b10dada753d3d0c6d4d
shado <shadowstakeus28@gmail.com>
2023-09-05 19:26:56 +0300

    feat: save values into database

--- server/server.js
+++ server/server.js
@@ -1,4 +1,4 @@
-const { Client } = require('pg')
+const {Client} = require('pg')
 const bodyParser = require('body-parser')
 const express = require('express')
 const cors = require('cors')
@@ -8,7 +8,7 @@ const app = express();
 const database_url = "http://localhost:5173";
 const access_url = database_url
 app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", access_url); 
+    res.header("Access-Control-Allow-Origin", access_url);
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
@@ -27,40 +27,39 @@ client.connect()
 // *** ------ GETS ------
 // GET * table names from database
 app.get('/get-table-names', (req, res) => {
-  try {
-      const query = `SELECT table_name
-      FROM information_schema.tables 
-      WHERE table_schema = 'public' 
-      AND table_catalog = '${client.database}'
-      AND table_type = 'BASE TABLE';`;
-      
-      client.query(query, (err, result) => {
-          if (err) {
-              throw err; 
-          }
-          res.send(result.rows);
-      });
-  } catch (error) {
-      console.error(error);
-      res.status(500).send('Internal Server Error');
-  }
+    try {
+        const query = `SELECT table_name
+                       FROM information_schema.tables
+                       WHERE table_schema = 'public'
+                         AND table_catalog = '${client.database}'
+                         AND table_type = 'BASE TABLE';`;
+
+        client.query(query, (err, result) => {
+            if (err) {
+                throw err;
+            }
+            res.send(result.rows);
+        });
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Internal Server Error');
+    }
 });
 
 
 // GET * column names from table
 app.get('/get-column-names', (req, res) => {
     const table_name = req.query.table
-    const query =  
-    `SELECT column_name, ordinal_position
-    FROM information_schema.columns
-    WHERE table_name = '${table_name}'
-    ORDER BY ordinal_position;`
+    const query =
+        `SELECT column_name, ordinal_position
+         FROM information_schema.columns
+         WHERE table_name = '${table_name}'
+         ORDER BY ordinal_position;`
     client.query(query, (err, result) => {
         if (err) {
             console.error(err);
             res.status(500).send('Error retrieving column names');
-        }
-        else {
+        } else {
             const rows = result.rows
             const columnNames = rows.map((row) => row.column_name);
             res.send(columnNames)
@@ -70,7 +69,8 @@ app.get('/get-column-names', (req, res) => {
 // GET * rows
 app.get('/get-all-data-from-table', (req, res) => {
     const table_name = req.query.table
-    const query = `SELECT * FROM ${table_name}`
+    const query = `SELECT *
+                   FROM ${table_name}`
     client.query(query, (err, result) => {
         const rows = result.rows
         res.send(rows)
@@ -81,58 +81,140 @@ app.get('/get-all-data-from-table', (req, res) => {
 // add row
 app.post('/add-empty-row', (req, res) => {
     const table_name = req.query.table
-    client.query(`INSERT INTO ${table_name} DEFAULT VALUES`)
+    client.query(`INSERT INTO ${table_name} DEFAULT
+                  VALUES`)
 })
 
 app.use(bodyParser.json())
 app.post('/delete-selected-rows', (req, res) => {
-    const recievedArray = req.body
-    const start = recievedArray[0]
-    const end = recievedArray[1]
-    // TODO const table_name = recievedArray[3]
-    const query = `DELETE FROM department WHERE department_id BETWEEN ${start} AND ${end}`
+    const receivedArray = req.body
+    const start = receivedArray[0]
+    const end = receivedArray[1]
+    // TODO const table_name = receivedArray[3]
+    const query = `DELETE
+                   FROM department
+                   WHERE department_id BETWEEN ${start} AND ${end}`
     client.query(query)
 })
 
+// app.post('/save-table', (req, res) => {
+//     const { tableName, table } = req.body;
+//     console.log(tableName, table);
+//
+//     //  database library Sequelize
+//     const Model = require(`/models/${tableName}`);
+//
+//     // Fetch the existing table from the database
+//     Model.findOne({ where: { tableName } })
+//       .then(existingTable => {
+//         if (existingTable) {
+//           // Compare the modified table with the existing one
+//           const isModified = compareTables(existingTable, table);
+//
+//           if (isModified) {
+//             // Update the table in the database
+//             existingTable.update(table)
+//               .then(() => {
+//                 console.log('Table updated successfully');
+//                 res.status(200).json({ message: 'Table updated successfully' });
+//               })
+//               .catch(error => {
+//                 console.error('Error updating table:', error);
+//                 res.status(500).json({ error: 'Failed to update table' });
+//               });
+//           } else {
+//             console.log('No changes in the table');
+//             res.status(200).json({ message: 'No changes in the table' });
+//           }
+//         } else {
+//           console.log('Table not found');
+//           res.status(404).json({ error: 'Table not found' });
+//         }
+//       })
+//       .catch(error => {
+//         console.error('Error fetching existing table:', error);
+//         res.status(500).json({ error: 'Failed to fetch existing table' });
+//       });
+//   });
+
 app.post('/save-table', (req, res) => {
-    const { tableName, table } = req.body;
-    console.log(tableName, table);
-  
-    //  database library Sequelize
-    const Model = require(`/models/${tableName}`);
-  
-    // Fetch the existing table from the database
-    Model.findOne({ where: { tableName } })
-      .then(existingTable => {
-        if (existingTable) {
-          // Compare the modified table with the existing one
-          const isModified = compareTables(existingTable, table);
-  
-          if (isModified) {
-            // Update the table in the database
-            existingTable.update(table)
-              .then(() => {
-                console.log('Table updated successfully');
-                res.status(200).json({ message: 'Table updated successfully' });
-              })
-              .catch(error => {
-                console.error('Error updating table:', error);
-                res.status(500).json({ error: 'Failed to update table' });
-              });
-          } else {
-            console.log('No changes in the table');
-            res.status(200).json({ message: 'No changes in the table' });
-          }
-        } else {
-          console.log('Table not found');
-          res.status(404).json({ error: 'Table not found' });
+    const requestData = req.body
+    const table_name = requestData.pop()[0]
+    // console.log("request data");
+    // console.log(requestData);
+    const query = `SELECT *
+                   FROM ${table_name}`
+    client.query(query, (err, result) => {
+        const databaseData = result.rows
+
+        let valuesToChange = []
+
+        for (let i = 0; i < databaseData.length; i++) {
+            const requestArrayData = requestData[i]
+            const databaseObjectData = databaseData[i]
+
+            let tempLoopValuesOfObject = []
+            for (const databaseObjectDataKey in databaseObjectData) {
+                let objVal = databaseObjectData[databaseObjectDataKey];
+                tempLoopValuesOfObject.push(objVal)
+            }
+
+
+            let shouldPush = false;
+            for (let j = 0; j < requestArrayData.length; j++) {
+                if (requestArrayData[j] !== tempLoopValuesOfObject[j])
+                    shouldPush = true
+            }
+
+            if (shouldPush === true)
+                valuesToChange.push(requestArrayData)
+
         }
-      })
-      .catch(error => {
-        console.error('Error fetching existing table:', error);
-        res.status(500).json({ error: 'Failed to fetch existing table' });
-      });
-  });
+
+        // console.log(valuesToChange);
+
+        // const values = [['new', 'values'], 30]; // $1 is replaced with ['new', 'values'], $2 is replaced with 30
+        // const query = 'UPDATE $1 SET data_array = $2 WHERE id = $3';
+
+        for (const array of valuesToChange) {
+
+            let composedQuery = ""
+
+            const id = array[0]
+
+            const qr =
+                `SELECT column_name, ordinal_position
+                 FROM information_schema.columns
+                 WHERE table_name = '${table_name}'
+                 ORDER BY ordinal_position;`
+
+            client.query(qr, (err, result) => {
+                const rows = result.rows
+                const columnNames = rows.map((row) => row.column_name);
+
+                composedQuery = `UPDATE ${table_name} SET `
+
+                for (let i = 0; i < columnNames.length; i++) {
+                    const columnName = columnNames[i];
+                    let value = array[i];
+                    if (typeof value === "string") value = `'${value}'`
+                    composedQuery += `${columnName} = ${value},`
+                }
+
+                composedQuery = composedQuery.substring(0, composedQuery.length - 1)
+                composedQuery += ` WHERE ${columnNames[0]} = ${id}`
+
+                client.query(composedQuery)
+            })
+
+        }
+
+
+    })
+    res.sendStatus(200)
+
+})
+
 
 // * ------ RUN APP ------
 app.listen(3000, () => {
