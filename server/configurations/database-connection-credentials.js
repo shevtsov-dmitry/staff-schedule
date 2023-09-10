286216403b57d183b19f7f673023045cfdad57bc
shado <shadowstakeus28@gmail.com>
2023-09-10 11:59:35 +0300

    refactor: create database-connection-credentials.js and use variable in client connection in server

---
+++ server/configurations/database-connection-credentials.js
@@ -0,0 +1,10 @@
+const databaseConnectionCredentials =
+    {
+        user: 'postgres',
+        host: 'localhost',
+        database: 'staff_schedule',
+        password: '123123',
+        port: 5432,
+    }
+
+    module.exports = databaseConnectionCredentials
--- server/server.js
+++ server/server.js
@@ -16,38 +16,22 @@ const {getDepartmentNames} = require("./GET/getDepartmentNames");
 const {getAvailableEmployees} = require("./GET/getAvailableEmployees");
 const {getShiftsTime} = require("./GET/getShiftsTime");
 const {assignEmployeeToShift} = require("./POST/assignEmployeeToShift");
+const databaseConnectionCredentials = require("./configurations/database-connection-credentials");
 const app = express();
-// access between web and server
-const access_url = "http://localhost:5173/"
 
-// app.use(cors({
-//     origin: access_url
-// }));
-// app.use(function (req, res, next) {
-//     res.header("Access-Control-Allow-Origin", access_url);
-//     // res.header("Access-Control-Allow-Origin", access_url + "$/index.html");
-//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//     next();
-// });
+// * ------ CORS POLICY ------
 app.use(cors({
     origin: '*'
 }));
 
-
 // * ------ CLIENT CONNECT TO DB ------
-const client = new Client({
-    user: 'postgres',
-    host: 'localhost',
-    database: 'staff_schedule',
-    password: '123123',
-    port: 5432,
-});
-
+const client = new Client(databaseConnectionCredentials);
 client.connect()
+
+// *
 app.use(bodyParser.json())
 
 // *** ------ GETs ------
-
 getTableNames(app, client)
 getColumnNames(app, client)
 getAllDataFromTable(app, client)
@@ -64,6 +48,7 @@ saveTable(app, client)
 deleteDepartment(app, client)
 verifyAdmin(app, client)
 assignEmployeeToShift(app, client)
+
 
 // * ------ RUN APP ------
 app.listen(3000, () => {
