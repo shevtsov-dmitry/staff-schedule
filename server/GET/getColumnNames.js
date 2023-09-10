c0c4d395c685cb681c6a14280f52a7cd5cf1d310
shado <shadowstakeus28@gmail.com>
2023-09-10 11:13:39 +0300

    refactor: create for each GET request its own file

---
+++ server/GET/getColumnNames.js
@@ -0,0 +1,23 @@
+function getColumnNames(app, client){
+    app.get('/get-column-names', (req, res) => {
+        const table_name = req.query.table
+        const query =
+            `SELECT column_name, ordinal_position
+         FROM information_schema.columns
+         WHERE table_name = '${table_name}'
+         ORDER BY ordinal_position;`
+        client.query(query, (err, result) => {
+            if (err) {
+                console.error(err);
+                res.status(500).send('Error retrieving column names');
+            } else {
+                const rows = result.rows
+                const columnNames = rows.map((row) => row.column_name);
+                res.send(columnNames)
+            }
+        })
+    })
+
+}
+
+module.exports = { getColumnNames}
--- server/server.js
+++ server/server.js
@@ -1,8 +1,12 @@
+
 const {Client} = require('pg')
 const bodyParser = require('body-parser')
 const express = require('express')
 const cors = require('cors')
 const {query} = require("express");
+const {getTableNames} = require("./GET/get-table-names");
+const {getColumnNames} = require("./GET/getColumnNames");
+const {getAllDataFromTable} = require("./GET/getAllDataFromTable");
 
 const app = express();
 // access between web and server
@@ -33,58 +37,12 @@ const client = new Client({
 
 client.connect()
 
-// *** ------ GETS ------
-// GET * table names from database
-app.get('/get-table-names', (req, res) => {
-    try {
-        const query = `SELECT table_name
-                       FROM information_schema.tables
-                       WHERE table_schema = 'public'
-                         AND table_catalog = '${client.database}'
-                         AND table_type = 'BASE TABLE';`;
+// *** ------ GETs ------
 
-        client.query(query, (err, result) => {
-            if (err) {
-                throw err;
-            }
-            res.send(result.rows);
-        });
-    } catch (error) {
-        console.error(error);
-        res.status(500).send('Internal Server Error');
-    }
-});
+getTableNames(app, client)
+getColumnNames(app, client)
+getAllDataFromTable(app, client)
 
-
-// GET * column names from table
-app.get('/get-column-names', (req, res) => {
-    const table_name = req.query.table
-    const query =
-        `SELECT column_name, ordinal_position
-         FROM information_schema.columns
-         WHERE table_name = '${table_name}'
-         ORDER BY ordinal_position;`
-    client.query(query, (err, result) => {
-        if (err) {
-            console.error(err);
-            res.status(500).send('Error retrieving column names');
-        } else {
-            const rows = result.rows
-            const columnNames = rows.map((row) => row.column_name);
-            res.send(columnNames)
-        }
-    })
-})
-// GET * rows
-app.get('/get-all-data-from-table', (req, res) => {
-    const table_name = req.query.table
-    const query = `SELECT *
-                   FROM ${table_name}`
-    client.query(query, (err, result) => {
-        const rows = result.rows
-        res.send(rows)
-    })
-})
 
 // *** ------ POSTS ------
 // add row
@@ -105,46 +63,6 @@ app.post('/delete-selected-rows', (req, res) => {
                    WHERE department_id BETWEEN ${start} AND ${end}`
     client.query(query)
 })
-
-// app.post('/save-table', (req, res) => {
-//     const { tableName, table } = req.body;
-//     console.log(tableName, table);
-//
-//     //  database library Sequelize
-//     const Model = require(`/models/${tableName}`);
-//
-//     // Fetch the existing table from the database
-//     Model.findOne({ where: { tableName } })
-//       .then(existingTable => {
-//         if (existingTable) {
-//           // Compare the modified table with the existing one
-//           const isModified = compareTables(existingTable, table);
-//
-//           if (isModified) {
-//             // Update the table in the database
-//             existingTable.update(table)
-//               .then(() => {
-//                 console.log('Table updated successfully');
-//                 res.status(200).json({ message: 'Table updated successfully' });
-//               })
-//               .catch(error => {
-//                 console.error('Error updating table:', error);
-//                 res.status(500).json({ error: 'Failed to update table' });
-//               });
-//           } else {
-//             console.log('No changes in the table');
-//             res.status(200).json({ message: 'No changes in the table' });
-//           }
-//         } else {
-//           console.log('Table not found');
-//           res.status(404).json({ error: 'Table not found' });
-//         }
-//       })
-//       .catch(error => {
-//         console.error('Error fetching existing table:', error);
-//         res.status(500).json({ error: 'Failed to fetch existing table' });
-//       });
-//   });
 
 app.post('/save-table', (req, res) => {
     const requestData = req.body
@@ -225,7 +143,7 @@ app.post('/save-table', (req, res) => {
 
 })
 
-app.post('/delete-dep',(req, res) => {
+app.post('/delete-dep', (req, res) => {
     const id = req.body.id
     const query = `call sp_DeleteDepartment(${id})`
     client.query(query, (err, result) => {
@@ -235,20 +153,21 @@ app.post('/delete-dep',(req, res) => {
 
 app.post(`/admin`, (req, res) => {
 
-    // let login = req.body.login
-    // let password = req.body.password
-    // const query = `SELECT id
-    //                FROM admins
-    //                WHERE login = ${login}
-    //                 AND password = ${password}`
-    // client.query(query, (err, result) => {
-    //     console.log(result);
-    // console.log(result.rows[0].id);
-    // if (result.rows[0].id === undefined) res.send(true)
-    // else res.send(false)
-    // })
-    let r = req.body
-    res.send(true)
+    console.log(req.body);
+    let login = req.body.login
+    let password = req.body.password
+    const query = `
+        SELECT id
+        FROM admins
+        WHERE login = ${login}
+          AND password = ${password}`;
+    client.query(query, (err, result) => {
+        // if (result.rows[0].id === undefined) res.send(true)
+        // else res.send(false)
+
+        res.send(true)
+    })
+
 
 })
 
@@ -386,6 +305,7 @@ app.post('/assign_employee_to_shift', (req, res) => {
             })
         }
     })
+
 
 
 
