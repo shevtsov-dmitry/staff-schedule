8424a9884e2c8c033425bc72798b6083d88845bd
shevtsov dmitry <shadowstakeus28@gmail.com>
2023-09-20 20:30:05 +0300

    feat: table columns now in russian lanuage

--- web/scripts/main.js
+++ web/scripts/main.js
@@ -200,6 +200,8 @@ async function constructTable(table_name) {
         });
 }
 
+
+
 // get column names
 async function getColumnNames(table_name) {
     let column_names = [];
@@ -211,7 +213,8 @@ async function getColumnNames(table_name) {
         .then(column_names_response => {
             for (const key in column_names_response) {
                 if (Object.hasOwnProperty.call(column_names_response, key)) {
-                    const element = column_names_response[key];
+                    let element = column_names_response[key];
+                    element = translateColumnNameIntoRussian(element)
                     column_names.push(element)
                 } else throw new Error(`Object does not have property ${key}`)
             }
@@ -616,3 +619,36 @@ function parseTableNameToRussian(tableName) {
 }
 
 
+function translateColumnNameIntoRussian(element) {
+    switch (element){
+        case 'login': element = 'логин'; break;
+        case 'password': element = 'пароль'; break;
+        case 'department_name': element = 'название'; break;
+        case 'department_budget': element = 'бюджет'; break;
+        case 'first_name': element = 'имя'; break;
+        case 'last_name': element = 'фамилия'; break;
+        case 'phone_number': element = 'номер телефона'; break;
+        case 'email': element = 'электронная почта'; break;
+        case 'hire_date': element = 'дата найма'; break;
+        case 'job_title': element = 'название'; break;
+        case 'job_description': element = 'описание'; break;
+        case 'job_requirements': element = 'требования'; break;
+        case 'location_name': element = 'название'; break;
+        case 'address_line_1': element = 'адрес'; break;
+        case 'address_line_2': element = 'дополнительный адрес'; break;
+        case 'city': element = 'город'; break;
+        case 'region': element = 'регион'; break;
+        case 'zip_code': element = 'почтовый код'; break;
+        case 'country': element = 'страна'; break;
+        case 'position_title': element = 'название'; break;
+        case 'position_description': element = 'описание'; break;
+        case 'required_hours_per_week': element = 'требуемое время работы'; break;
+        case 'salary': element = 'зарплата'; break;
+        case 'hourly_rate': element = 'почасовая ставка'; break;
+        case 'bonus_coefficient': element = 'надбавка'; break;
+        case 'shift_start_time': element = 'начало смены'; break;
+        case 'shift_end_time': element = 'конец смены'; break;
+    }
+
+    return element;
+}
