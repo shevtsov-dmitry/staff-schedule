fd2a6bba49d9a2d12e7019837e26635fc569b4fb
shevtsov dmitry <shadowstakeus28@gmail.com>
2023-10-11 11:47:35 +0300

    feat: improve answer to send after successfully completed shift and employee tables update

--- server/POST/assignEmployeeToShift.js
+++ server/POST/assignEmployeeToShift.js
@@ -25,6 +25,7 @@ function assignEmployeeToShift(app, client) {
             WHERE shift_start_time = '${shift_start_time}'
               AND shift_end_time = '${shift_end_time}'
         `;
+
         client.query(queryToSelectEmployeeId, (err, result) => {
             if (err) {
                 console.error(err);
@@ -44,15 +45,15 @@ function assignEmployeeToShift(app, client) {
 
                         const callProcedureQuery = `
                         CALL assign_employee_to_shift(${selected_shift_schedule_id},${selected_employee_id})
-                    `
+                    `;
                         client.query(callProcedureQuery, (err, re) => {
                             if (err) {
                                 console.error(err);
                                 res.status(500).send("Error retrieving changing employee's shift");
                             } else {
                                 res.status(200).send({
-                                        answer: ` Теперь на смене с номером ${selected_shift_schedule_id}` +
-                                            ` работает сотрудник с идентификатором ${selected_employee_id}`
+                                        answer: ` Теперь на смену с ${shift_start_time} до ${shift_end_time} ` +
+                                            `назначен сотрудник ${first_name} ${last_name}.`
                                     }
                                 )
                             }
