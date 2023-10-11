1d583ec7c7102cd76271c1e9cd56967f00ce9139
shevtsov dmitry <shadowstakeus28@gmail.com>
2023-10-11 17:49:29 +0300

    feat: add vital xml elements to parser.

--- server/UTILS/downloadReportInFormat.js
+++ server/UTILS/downloadReportInFormat.js
@@ -56,7 +56,11 @@ function downloadReportInFormat(app, client) {
                 case "xml": {
                     res.setHeader('Content-Type', 'text/xml');
                     res.setHeader('Content-Disposition', 'attachment; filename="report.csv"');
-                    res.send(toXML(answerObject))
+                    const XML = `<?xml version="1.0" encoding="UTF-8" ?>
+                                        <root>
+                                            ${toXML(answerObject)}
+                                        </root>`;
+                    res.send(XML)
                     break
                 }
                 case "csv": {
