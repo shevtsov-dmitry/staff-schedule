44ac2fe996d9e78223c0fce1288fec23464a3edf
shevtsov-dmitry <shadowstakeus28@gmail.com>
2023-11-26 17:04:46 +0300

    refactor: decomposition of downloadReportInFormat.js

--- server/UTILS/downloadReportInFormat.js
+++ server/UTILS/downloadReportInFormat.js
@@ -1,4 +1,4 @@
-const { toXML } = require("jstoxml");
+const {toXML} = require("jstoxml");
 const {Parser} = require("@json2csv/plainjs");
 
 // constructors
@@ -18,78 +18,69 @@ function EmployeeShift(shiftStartTime, shiftEndTime, assignedEmployeeList) {
     this.assignedEmployeeList = assignedEmployeeList;
 }
 
-// mutual query
-// TODO hardcoded shift time values. Better to change as incoming params
-const QUERY = `SELECT *
-                       FROM show_employees_shifts
-                       WHERE shift_start_time = '09:00:00'
-                         AND shift_end_time = '18:00:00'`;
-
+// main function
 function downloadReportInFormat(app, client) {
     app.get("/download-report-in-format", (req, res) => {
         const format = req.query.format
-        //create JSON object
-
         client.query(QUERY, (err, result) => {
-
-            const employeesNames = []
-            for (let el of result.rows) {
-                const employeeName = `${el.first_name} ${el.last_name}`
-                // console.log("NAME: " + employeeName)
-                employeesNames.push(employeeName)
-            }
-            const shift = new Shift(result.rows[0].shift_start_time, result.rows[0].shift_end_time)
-            const employeeShift = new EmployeeShift(shift.shiftStartTime, shift.shiftEndTime, employeesNames)
-            const answerObject = {
-                shift_start_time: employeeShift.shiftStartTime,
-                shift_end_time: employeeShift.shiftEndTime,
-                assigned_employee_list: employeeShift.assignedEmployeeList
-            }
-
-            switch (format) {
-                case "json": {
-                    res.setHeader('Content-Type', 'text/json');
-                    res.setHeader('Content-Disposition', 'attachment; filename="report.csv"');
-                    res.send(answerObject)
-                    break
-                }
-                case "xml": {
-                    res.setHeader('Content-Type', 'text/xml');
-                    res.setHeader('Content-Disposition', 'attachment; filename="report.csv"');
-                    const XML = `<?xml version="1.0" encoding="UTF-8" ?><root>${toXML(answerObject)}</root>`;
-                    res.send(XML)
-                    break
-                }
-                case "csv": {
-                    const opts = {};
-                    const parser = new Parser(opts);
-                    const csv = parser.parse(answerObject);
-                    res.setHeader('Content-Type', 'text/csv');
-                    res.setHeader('Content-Disposition', 'attachment; filename="report.csv"');
-                    res.send(csv)
-                    break
-                }
-                case "txt": {
-                    // Convert the query result to an array of objects
-                    const data = result.rows;
-                    // Convert the data to a custom text format with "|" delimiters
-                    let textData = ""
-                    for (const el of data) {
-                        textData += `${el.shift_start_time}|${el.shift_end_time}|${el.first_name} ${el.last_name}\n`
-                    }
-                    textData = textData.substring(0, textData.length-1) // get rid of last element \n
-                        res.setHeader('Content-Type', 'text/plain');
-                        res.setHeader('Content-Disposition', 'attachment; filename="report.csv"');
-                        res.send(textData)
-                    break
-                }
-            }
-
-
+            const JSONReport = composeJSONReport(result)
+            const report = parseReportToRequestedFormat(JSONReport, format)
+            sendReportFileAsResponse(report, format, res);
         })
-
     })
 }
-
 module.exports = {downloadReportInFormat}
+
+// mutual query
+const QUERY = `SELECT *
+               FROM show_employees_shifts
+               WHERE shift_start_time = '09:00:00'
+                 AND shift_end_time = '18:00:00'`;
+
+function sendReportFileAsResponse(report, format, res) {
+    if(format === "Unsupported format."){
+        res.sendStatus(400).body("Unsupported format.")
+    }
+    res.setHeader('Content-Type', `text/${format}`);
+    res.setHeader('Content-Disposition', `attachment; filename="${format}"`);
+    res.send(report)
+}
+
+function parseTextReport(answerObject) {
+    let textData = ""
+    for (let name of answerObject.assigned_employee_list) {
+        textData += `${answerObject.shift_start_time}|${answerObject.shift_end_time}|${name}\n`
+    }
+    return textData.substring(0, textData.length - 1) // get rid of last element \n
+}
+
+function parseReportToRequestedFormat(JSONReport, format) {
+    if (format === "json")
+        return JSONReport
+    if (format === "xml")
+        return `<?xml version="1.0" encoding="UTF-8" ?><root>${toXML(JSONReport)}</root>`
+    if (format === "txt")
+        return parseTextReport(JSONReport);
+    if (format === "csv") {
+        const opts = {};
+        const parser = new Parser(opts);
+        return parser.parse(JSONReport);
+    }
+    return "Unsupported format.";
+}
+
+function composeJSONReport(result) {
+    const employeesNames = []
+    for (let el of result.rows) {
+        const employeeName = `${el.first_name} ${el.last_name}`
+        employeesNames.push(employeeName)
+    }
+    const shift = new Shift(result.rows[0].shift_start_time, result.rows[0].shift_end_time)
+    const employeeShift = new EmployeeShift(shift.shiftStartTime, shift.shiftEndTime, employeesNames)
+    return {
+        shift_start_time: employeeShift.shiftStartTime,
+        shift_end_time: employeeShift.shiftEndTime,
+        assigned_employee_list: employeeShift.assignedEmployeeList
+    }
+}
 
