9d2e1342165e7be2efb30a78afd36be1a5124226
shevtsov-dmitry <shadowstakeus28@gmail.com>
2023-11-27 13:29:35 +0300

    feat: add MAIL_CREDENTIALS template

---
+++ server/configurations/MAIL_CREDENTIALS.js
@@ -0,0 +1,6 @@
+const MAIL_CREDENTIALS = {
+    // insert your credentials or better use environment variables for better security
+    MAIL: "",
+    PASSWORD: ""
+}
+module.exports = {MAIL_CREDENTIALS}
