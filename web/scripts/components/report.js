23abf684f87a0ab492706ada95cf9910193c9be6
shevtsov-dmitry <shadowstakeus28@gmail.com>
2023-11-27 21:27:49 +0300

    refactor: complete improving report component

--- web/scripts/components/report.js
+++ web/scripts/components/report.js
@@ -1,22 +1,90 @@
 import Handsontable from "handsontable";
-import {host} from "../configurations/hostServer";
-import {
-    choose_table_btn,
-    placeholder_text,
-    procedures_flex_view,
-    show_procedures_button,
-    table_names_list
-} from "./table";
+import {host} from "../configurations/HOST_SERVER";
 
-export function initReportComponent(){
-    // compose report
-    const reportComposeBtn = document.querySelector("#btn-report-employees-and-shifts")
-    const reportTableDOM = document.querySelector('.report-table')
-    const emailOperationsDiv = document.querySelector(".email-operations-div")
-    const btnDownloadReport = document.querySelector('#download-report-btn')
+const btnDownloadReport = document.querySelector('#download-report-btn')
+const btnActivateTimer = document.querySelector("#btn-download-with-timer")
+
+function email() {
+    const mailStorage = document.querySelector('#storage-email-announcer')
+    const mailSenderMessage = document.querySelector("#announce-message-on-email-send")
+    const storageEmailAnnouncer = document.querySelector("#storage-email-announcer")
+    const storageEmailShowBtn = document.querySelector("#storage-email-show-btn")
+    const sendReportToEmailButton = document.querySelector("#send-report-to-email-btn")
+
+    fetch(`${host}` + '/get-mail-storage')
+        .then(response => response.text())
+        .then(mailName => {
+            mailStorage.textContent = mailName
+        })
+
+    function displayAndHideReceiverEmailAnnouncerOnClick() {
+        const condition = storageEmailAnnouncer.style.display === "initial"
+        storageEmailAnnouncer.style.display = condition ? "none" : "initial"
+    }
+
+    storageEmailShowBtn.addEventListener('click', () => {
+        displayAndHideReceiverEmailAnnouncerOnClick();
+    })
+
+    function displayOperationStatusMessage(message) {
+        const hideOnClick = () => mailSenderMessage.addEventListener('click', () => {
+            mailSenderMessage.style.display = "none"
+        })
+
+        mailSenderMessage.style.display = "initial"
+        mailSenderMessage.textContent = message
+        hideOnClick();
+
+    }
+
+    sendReportToEmailButton.addEventListener("click", () => {
+        fetch(`${host}` + "/send-email", {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        })
+            .then(response => response.text())
+            .then(answer => {
+                displayOperationStatusMessage(answer)
+            })
+    })
+
+    const getEmailDataBtn = document.querySelector("#get-email-data-btn")
+    const xmlReportArticle = document.querySelector("#xml-report")
+    const jsonReportArticle = document.querySelector("#json-report")
+
+    getEmailDataBtn.addEventListener('click', () => {
+        displayContentNameHeaders();
+        fetch(`${host}` + "/get-data-from-email")
+            .then(response => response.json())
+            .then(data => {
+                xmlReportArticle.innerText = stringifyXML(data.xml);
+                jsonReportArticle.innerHTML = JSON.stringify(data.json)
+            })
+        function displayContentNameHeaders() {
+            const xmlPlaceholder = document.querySelector("#XML-placeholder")
+            const jsonPlaceholder = document.querySelector("#JSON-placeholder")
+            xmlPlaceholder.style.display = "initial"
+            jsonPlaceholder.style.display = "initial"
+        }
+    })
+
+    function stringifyXML(rawXML) {
+        let string = rawXML
+        string = string.replace("?>", "?>\n")
+        string = string.replace("<root>", "<root>\n\t")
+        string = string.replace("</shift_start_time>", "</shift_start_time>\n\t")
+        string = string.replace("</shift_end_time>", "</shift_end_time>\n\t")
+        string = string.replaceAll("</assigned_employee_list>", "</assigned_employee_list>\n\t")
+        string = string.substring(0, string.length - 8)
+        string += "</root>"
+        return string
+    }
+}
+
+function downloadReport() {
     const ulDownloadFormats = document.querySelector('.ul-download-formats')
-    const btnActivateTimer = document.querySelector("#btn-download-with-timer")
-    const save_btn = document.querySelector('#save')
 
     let countTimesBtnDownloadReportClicked = 0
     btnDownloadReport.addEventListener('click', () => {
@@ -28,50 +96,6 @@ export function initReportComponent(){
             countTimesBtnDownloadReportClicked = 0
         }
     })
-
-    choose_table_btn.addEventListener('click', () => {
-        placeholder_text.innerHTML = ""
-        table_names_list.style.display = "flex"
-    })
-
-    show_procedures_button.addEventListener('click', () => {
-        procedures_flex_view.style.display = 'flex'
-    })
-
-
-
-// this element needed for not to dupe report each time @reportComposeBtn pressed
-    let reportBtnPressCounter = 0
-    function displayAdditionalActionButtonsForReportBlock() {
-        btnActivateTimer.style.display = 'block'
-        emailOperationsDiv.style.display = 'flex'
-    }
-    reportComposeBtn.addEventListener('click', () => {
-        // delete old table if pressed again
-        if (reportBtnPressCounter === 1) {
-            reportTableDOM.rootElement.style.display = 'none'
-        }
-        reportBtnPressCounter++
-
-        displayAdditionalActionButtonsForReportBlock();
-
-        fetch(`${host}/get-employees-names-and-their-shifts?time="09:00:00"`, {
-            method: "GET",
-            headers: {'Content-Type': 'application/json '},
-        })
-            .then(res => res.json())
-            .then(data => {
-                new Handsontable(reportTableDOM, {
-                    data: data,
-                    rowHeaders: true,
-                    colHeaders: ["имя", "фамилия", "начало смены", "конец смены"],
-                    height: 'auto',
-                    licenseKey: 'non-commercial-and-evaluation' // for non-commercial use only
-                })
-                btnDownloadReport.style.display = "block"
-            })
-    })
-
     const downloadFile = (fileExtensionName) => {
         let url = "download-report-in-format?format="
         let fileName = ""
@@ -139,81 +163,53 @@ export function initReportComponent(){
             }
         }
         timesBtnActivateTimerWasClicked++;
-
     })
+}
 
-    const mailStorage = document.querySelector('#storage-email-announcer')
-    const mailSenderMessage = document.querySelector("#announce-message-on-email-send")
-    const storageEmailAnnouncer = document.querySelector("#storage-email-announcer")
-    const storageEmailShowBtn = document.querySelector("#storage-email-show-btn")
-    const sendReportToEmailButton = document.querySelector("#send-report-to-email-btn")
+function displayAdditionalActionButtonsForReportBlock() {
+    const emailOperationsDiv = document.querySelector(".email-operations-div")
+    btnActivateTimer.style.display = 'block'
+    emailOperationsDiv.style.display = 'flex'
+}
 
-    fetch(`${host}` + '/get-mail-storage')
-        .then(response => response.text())
-        .then(mailName => {
-            mailStorage.textContent = mailName
+function fetchShiftScheduleReport(reportTableDOM) {
+    function createNewReportTable(data) {
+        return new Handsontable(reportTableDOM, {
+            data: data,
+            rowHeaders: true,
+            colHeaders: ["имя", "фамилия", "начало смены", "конец смены"],
+            height: 'auto',
+            licenseKey: 'non-commercial-and-evaluation' // for non-commercial use only
         })
+    }
 
-    function displayAndHideReceiverEmailAnnouncerOnClick() {
-        if (storageEmailAnnouncer.style.display === "initial") {
-            storageEmailAnnouncer.style.display = "none"
-        } else {
-            storageEmailAnnouncer.style.display = "initial"
+    fetch(`${host}/get-employees-names-and-their-shifts?time="09:00:00"`, {
+        method: "GET",
+        headers: {'Content-Type': 'application/json '},
+    })
+        .then(res => res.json())
+        .then(data => {
+            createNewReportTable(data);
+            btnDownloadReport.style.display = "block"
+        })
+}
+
+function displayReport() {
+    const reportTableDOM = document.querySelector('.report-table')
+    const reportComposeBtn = document.querySelector("#btn-report-employees-and-shifts")
+
+    let isReportBtnPressedOnce = false
+    reportComposeBtn.addEventListener('click', () => {
+        if(!isReportBtnPressedOnce){
+            fetchShiftScheduleReport(reportTableDOM)
         }
-    }
-
-    storageEmailShowBtn.addEventListener('click', () => {
-        displayAndHideReceiverEmailAnnouncerOnClick();
+        displayAdditionalActionButtonsForReportBlock();
+        isReportBtnPressedOnce = true
     })
+}
 
-    function temporaryDisplayOperationStatusMessage(message) {
-        mailSenderMessage.style.display = "initial"
-        mailSenderMessage.textContent = message
-        mailSenderMessage.addEventListener('click', ()=>{
-            mailSenderMessage.style.display = "none"
-        })
-    }
-
-    sendReportToEmailButton.addEventListener("click", () => {
-        fetch(`${host}` + "/send-email", {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-        })
-            .then(response => response.text())
-            .then(answer => {
-                temporaryDisplayOperationStatusMessage(answer)
-            })
-    })
-
-    const getEmailDataBtn = document.querySelector("#get-email-data-btn")
-    const xmlReportArticle = document.querySelector("#xml-report")
-    const jsonReportArticle = document.querySelector("#json-report")
-
-    getEmailDataBtn.addEventListener('click', () => {
-
-        const xmlPlaceholder = document.querySelector("#XML-placeholder")
-        const jsonPlaceholder = document.querySelector("#JSON-placeholder")
-        xmlPlaceholder.style.display = "initial"
-        jsonPlaceholder.style.display = "initial"
-        fetch(`${host}` + "/get-data-from-email")
-            .then(response => response.json())
-            .then(data => {
-                xmlReportArticle.innerText = stringifyXML(data.xml);
-                jsonReportArticle.innerHTML = JSON.stringify(data.json)
-            })
-    })
-
-    function stringifyXML(rawXML) {
-        let string = rawXML
-        string = string.replace("?>", "?>\n")
-        string = string.replace("<root>", "<root>\n\t")
-        string = string.replace("</shift_start_time>", "</shift_start_time>\n\t")
-        string = string.replace("</shift_end_time>", "</shift_end_time>\n\t")
-        string = string.replaceAll("</assigned_employee_list>", "</assigned_employee_list>\n\t")
-        string = string.substring(0, string.length - 8)
-        string += "</root>"
-        return string
-    }
+export async function initReportComponent() {
+    displayReport();
+    downloadReport();
+    email();
 }
