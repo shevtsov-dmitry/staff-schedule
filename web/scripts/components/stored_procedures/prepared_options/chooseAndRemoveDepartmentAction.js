da525628cf9705b32aad2db8a1f2a2f04870003a
shevtsov-dmitry <shadowstakeus28@gmail.com>
2023-12-10 20:40:56 +0300

    refactor: chooseAndRemoveDepartmentAction.js

--- web/scripts/components/stored_procedures/prepared_options/chooseAndRemoveDepartmentAction.js
+++ web/scripts/components/stored_procedures/prepared_options/chooseAndRemoveDepartmentAction.js
@@ -1,24 +1,30 @@
 import {host} from "../../../configurations/HOST_SERVER";
 import {chooseAndRemoveDepartment, fillLi, proceduresUl} from "../storedProcedures";
 
-export function chooseAndRemoveDepartmentAction(){
-    proceduresUl.children[1].addEventListener('click', () => {
-        fillLi(proceduresUl.children[1], `${host}/get-department-names`)
+function fetchRequestToDeleteDepartment(id) {
+    return new Promise(resolve => {
+        fetch(`${host}/delete-dep?id=${id}`, {
+            method: "DELETE",
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({id: id})
+        }).then(()=>resolve())
+    })
+}
 
-        setTimeout(() => {
-            for (let child of chooseAndRemoveDepartment.children) {
-                child.addEventListener('click', () => {
-                    const id = child.innerHTML.replaceAll(/[^\d]/g, "")
-                    fetch(`${host}/delete-dep?id=${id}`, {
-                        method: "POST",
-                        headers: {
-                            'Content-Type': 'application/json'
-                        },
-                        body: JSON.stringify({id: id})
-                    }).then(() => {
-                    })
-                })
-            }
-        }, 200)
+async function sendRequestToDeleteDepartment() {
+    for (let child of chooseAndRemoveDepartment.children) {
+        child.addEventListener('click', () => {
+            const id = child.innerHTML.replaceAll(/[^\d]/g, "")
+            fetchRequestToDeleteDepartment(id);
+        })
+    }
+}
+
+export function chooseAndRemoveDepartmentAction() {
+    proceduresUl.children[1].addEventListener('click', async () => {
+        await fillLi(proceduresUl.children[1], `${host}/get-department-names`)
+        await sendRequestToDeleteDepartment();
     })
 }
